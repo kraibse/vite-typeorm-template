33ad37d5e363a00ad34eadb56895c30096e8c474
kraibse <33869366+kraibse@users.noreply.github.com>
2024-11-25 10:44:22 +0100

    Installed tailwind + Added a mockup backlog page

---
+++ vite-frontend/src/components/BacklogDetails.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import UserComment from './UserComment.vue';
+</script>
+
+<template>
+  <h1 class="text-left mb-4">The Addams Family</h1>
+
+  <div class="text-left flex flex-col gap-y-1 pb-4">
+    <div class="flex justify-start gap-x-1">
+      <p class="rounded-full bg-slate-600 px-3 w-fit">Movie</p>
+      <p class="rounded-full bg-slate-600 px-3 w-fit">2h 31m</p>
+      <p class="rounded-full bg-green-600 px-3 w-fit">Completed</p>
+    </div>
+  </div>
+
+    <div class="flex flex-row gap-x-2 w-full">
+      <div class="h-40 w-full rounded bg-slate-600 my-4 h-80 max-w-40"></div>
+      <div class="h-40 w-full rounded bg-slate-600 my-4 h-80 max-w-40"></div>
+      <div class="h-40 w-full rounded bg-slate-600 my-4 h-80 max-w-40"></div>
+  </div>
+
+  <div class="text-left">
+    <h2 class="text-xl">Description</h2>
+    <p class="pt-2 leading-6 text-gray-400">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor
+      sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet
+      consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
+      adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor
+      sit amet consectetur adipisicing elit.
+    </p>
+  </div>
+
+  <div class="flex gap-x-2 mt-4">
+    <button class="my-2 py-1 bg-indigo-600 rounded">Edit Details</button>
+    <button class="my-2 py-1 bg-indigo-600 rounded">Update Status</button>
+  </div>
+
+  <hr class="my-8" />
+
+  <div class="text-left">
+    <h2 class="mb-4">Comment as <span class="">kraibse</span></h2>
+    <textarea
+      name=""
+      id=""
+      rows="10"
+      class="w-full p-2"
+      placeholder="Enter your comment"
+    ></textarea>
+    <button class="my-2 py-1 bg-indigo-600 rounded">Send comment</button>
+  </div>
+
+  <div id="commentlist" class="mt-8 flex flex-col gap-y-4 text-left">
+    <h2>Comment Log</h2>
+
+    <UserComment />
+    <UserComment />
+    <UserComment />
+    <UserComment />
+    <UserComment />
+
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ vite-frontend/src/components/UserComment.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+    <div class="shadow rounded bg-slate-700 p-4 pb-2">
+      <div class="flex flex-row items-center">
+            <div class="avatar rounded-full w-8 h-8 bg-white mr-2"></div>
+            
+            <div class="flex w-full justify-between">
+                <p>kraibse commented</p>
+                <p class="text-gray-400 text-sm">2024-11-25 at 19:24</p>
+            </div>
+      </div>
+
+      <div class="pt-3">
+        <p class="text-gray-400">
+            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem IpsumLorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum.
+        </p>
+
+        <div class="mt-3 mb-1 h-fit">
+            <div class="h-fit w-fit bg-slate-800 rounded bg-cover">
+                <img src="https://picsum.photos/200/300" alt="" class="bg-cover">
+            </div>
+        </div>
+
+        <div class="">
+            <button class="my-2 py-1 bg-indigo-600 rounded">Reply</button>
+        </div>
+      </div>
+    </div>
+
+</template>
